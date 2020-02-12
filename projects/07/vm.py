ff3a1501e59b60e728aac1ff9a810d7ad0df590a
Justin Okamoto <jdokamoto@gmail.com>
2020-02-12 00:12:33 -0800

    Add logic for eq operation

--- projects/07/vm.py
+++ projects/07/vm.py
@@ -37,6 +37,8 @@ def gen(in_stream, out_stream):
                 out_stream.write("@SP\n")
                 out_stream.write("M=M+1\n")
                 continue
+        elif words[0] == 'pop':
+            pass
         elif words[0] == 'add':
             out_stream.write("@SP\n")
             out_stream.write("A=M-1\n")
@@ -46,8 +48,83 @@ def gen(in_stream, out_stream):
             out_stream.write("@SP\n")
             out_stream.write("M=M-1")
             continue
-        elif words[0] == 'pop':
+        elif words[0] == 'sub':
+            # @SP
+            # A=M-1
+            # D=M
+            # A=A-1
+            # M=D-M
+            # @SP
+            # M=M=1
             pass
+        elif words[0] == 'gt':
+            pass
+        elif words[0] == 'lt':
+            pass
+            # @SP
+            # A=M-1
+            # D=M
+            # A=A-1
+            # if D and M
+        elif words[0] == 'eq':
+            # Decrement stack pointer
+            out_stream.write("@SP\n")
+            out_stream.write("M=M-1\n")
+            out_stream.write("A=M\n")
+            out_stream.write("D=M\n") # set value top of stack
+            out_stream.write("A=A-1\n")
+            out_stream.write("D=D-M\n")
+            out_stream.write("@BRANCH_TRUE\n")
+            out_stream.write("D;JEQ\n")
+            out_stream.write("@SP\n")
+            out_stream.write("A=M-1\n")
+            out_stream.write("M=0\n")
+            out_stream.write("@END_BRANCH\n")
+            out_stream.write("0;JMP\n")
+            out_stream.write("(BRANCH_TRUE)\n")
+            out_stream.write("@SP\n")
+            out_stream.write("A=M-1\n")
+            out_stream.write("M=-1\n")
+            out_stream.write("(END_BRANCH)\n")
+            continue
+        elif words[0] == 'neg':
+            # @SP
+            # A=M-1
+            # M=-M
+            pass
+        elif words[0] == 'and':
+            # @SP
+            # A=M-1
+            # D=M
+            # A=A-1
+            # M=D&M
+            # @SP
+            # M=M=1
+            pass
+        elif words[0] == 'or':
+            # @SP
+            # A=M-1
+            # D=M
+            # A=A-1
+            # M=D|M
+            # @SP
+            # M=M=1
+            pass
+        elif words[0] == 'not':
+            # @SP
+            # A=M-1
+            # M=!M
+            pass
+# add x + y integer
+# sub x - y integer
+# neg -y integer
+# eq x==0 boolean
+# gt x > y boolean
+# lt x < y boolean
+# and x and y boolean (bitwise)
+# or x or y boolean (bitwise)
+# not not x boolean (bitwise)
+
 
 def main():
     parser = argparse.ArgumentParser(description="Generate HACK assembly")
